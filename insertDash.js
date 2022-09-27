997ec54f1b5bc4f35baf2bc0064caf40e8a59e0f
pankajjossy1 <50371645+pankajjossy1@users.noreply.github.com>
2022-09-27 14:23:47 +0530

    insertDash
    insertDash between two even numbers

---
+++ insertDash.js
@@ -0,0 +1,12 @@
+const num = 25468;
+const str = num.toString();
+console.log(str);
+const result = [0, "-", str[0]];
+for (let i = 1; i < str.length; i++) {
+  if (str[i - 1] % 2 === 0 && str[i] % 2 === 0) {
+    result.push("-", str[i]);
+  } else {
+    result.push(str[i]);
+  }
+}
+console.log(result.join(""));
